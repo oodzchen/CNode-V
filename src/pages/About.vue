f3ef1411fe12368a06a932d03b9e5334c6e6fe1e
oodzchen <oodzchen@gmail.com>
2019-01-11 19:14:00 +0800

    更新关于页面

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -12,7 +12,8 @@
       </v-card-title>
       <v-divider></v-divider>
       <v-card-text>
-        <p>本项目主要基于Vuetify开发，同时要感谢cnodejs.org提供的API。Android平台建议使用Chrome浏览器打开，iOS平台建议使用Safari浏览器打开，点击“添加到主屏幕”即可安装独立的PWA应用。</p>
+        <p>本项目主要基于Vuetify开发，同时要感谢cnodejs.org提供的API。</p>
+        <p>Android平台建议使用Chrome浏览器打开，iOS平台建议使用Safari浏览器，点击“添加到主屏幕”即可安装独立的PWA应用。</p>
         <p>开发者：<a target="_blank" href="https://github.com/oodzchen">oodzchen</a></p>
         <p>源码：<a target="_blank" href="https://github.com/oodzchen/CNode-V">https://github.com/oodzchen/CNode-V</a></p>
       </v-card-text>
