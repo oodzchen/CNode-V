e4dae45736f6de015479596019790c03bb372b7a
oodzchen <oodzchen@gmail.com>
2019-07-20 18:27:08 +0800

    解决netlify的路由问题

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -100,6 +100,10 @@ const webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../_redirects'),
+        to: config.build.assetsRoot
       }
     ]),
     // service worker caching
